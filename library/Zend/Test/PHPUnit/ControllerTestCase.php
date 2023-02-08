6f1101b49a03146dc58877130f76b75b738c0f4a
himawijaya <13044293+himawijaya@users.noreply.github.com>
2023-02-08 10:38:15 +0700

    fix: UnitTest php.8

--- library/Zend/Test/PHPUnit/ControllerTestCase.php
+++ library/Zend/Test/PHPUnit/ControllerTestCase.php
@@ -44,7 +44,8 @@ require_once 'Zend/Registry.php';
  * @copyright  Copyright (c) 2005-2015 Zend Technologies USA Inc. (http://www.zend.com)
  * @license    http://framework.zend.com/license/new-bsd     New BSD License
  */
-abstract class Zend_Test_PHPUnit_ControllerTestCase extends PHPUnit_Framework_TestCase
+use PHPUnit\Framework\TestCase;
+abstract class Zend_Test_PHPUnit_ControllerTestCase extends TestCase
 {
     /**
      * @var mixed Bootstrap file path or callback
